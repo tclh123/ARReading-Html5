67d28086a5bc19d23bac1918bb92cd4e1f947e16
tclh123 <tclh123@gmail.com>
2013-06-24 18:17:18 +0800

    reconstruct Data.js

---
+++ site/Js/Config.js
@@ -0,0 +1,15 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: tclh123
+ * Date: 13-6-24
+ * Time: 下午5:32
+ */
+
+
+//DEBUG = true;     // global var for JSAR
+var THRESHOLD = 90;
+var MARKER_SIZE = 120;
+
+//webcam尺寸
+var HEIGHT = 480;
+var WIDTH = HEIGHT*4/3;
--- site/Js/Data.js
+++ site/Js/Data.js
@@ -8,7 +8,7 @@
 
 AR.Data = function(Models) {
 
-
+    // 数据
     var pages = [
 
         // page
@@ -16,77 +16,182 @@ AR.Data = function(Models) {
             markerId: 0,
             models: [
                 {
-                    type: AR.Models.ModelType.video,
-                    width: 400 *672/378,
-                    height: 400,
-
-
-
+                    modelType: AR.Models.ModelType.video,
+                    modelWidth: 400 *672/378,
+                    modelHeight: 400,
+                    modelLeftTopX: 0,
+                    modelLeftTopY: 339,
+                    markerLeftTopX: 550,
+                    markerLeftTopY: 773,
+                    markerSize: 120,    // 在硬质媒体上，印有marker的大小
+                    attach: {
+                        src: 'Uploads/Movie/ImagineCup.mp4',
+                        width: 672,
+                        height: 378,
+                        isLoop: true
+                    },
+                    clickEventCallback: function(e){    // TODO: 要上线的话，这里要提供固定的几种clickEventCallback。
+                        if(e.clickCount%2)
+                            e.domElement.play();
+                        else
+                            e.domElement.pause();
+                    }
                 }
+            ]
+        },
+
+        // page
+        {
+            markerId: 1,
+            models: [
+                {
+                    modelType: AR.Models.ModelType.audio,
+                    modelWidth: 123,
+                    modelHeight: 129,
+                    modelLeftTopX: 20,
+                    modelLeftTopY: 221,
+                    markerLeftTopX: 400,
+                    markerLeftTopY: 615,
+                    markerSize: 80,
+                    attach: {
+                        src: 'Uploads/Audio/noun.mp3',
+                        isAutoplay: false,
+                        imageTexSrc: 'Uploads/Image/blank.png'
+                    },
+                    clickEventCallback: function(e){
+                        if(e.clickCount%2){
+                            e.domElement.play();
+                            e.obj.material.map =  THREE.ImageUtils.loadTexture('Uploads/Image/continue.png');
+                        }
+                        else{
+                            e.obj.material.map =  THREE.ImageUtils.loadTexture('Uploads/Image/blank.png');
+                        }
+                    }
+                },
+
+                {
+                    modelType: AR.Models.ModelType.audio,
+                    modelWidth: 116,
+                    modelHeight: 137,
+                    modelLeftTopX: 383,
+                    modelLeftTopY: 262,
+                    markerLeftTopX: 400,
+                    markerLeftTopY: 615,
+                    markerSize: 80,
+                    attach: {
+                        src: 'Uploads/Audio/pronoun.mp3',
+                        isAutoplay: false,
+                        imageTexSrc: 'Uploads/Image/blank.png'
+                    },
+                    clickEventCallback: function(e){
+                        if(e.clickCount%2){
+                            e.domElement.play();
+                            e.obj.material.map =  THREE.ImageUtils.loadTexture('Uploads/Image/continue.png');
+                        }
+                        else{
+                            e.obj.material.map =  THREE.ImageUtils.loadTexture('Uploads/Image/blank.png');
+                        }
+                    }
+                },
+
+                {
+                    modelType: AR.Models.ModelType.audio,
+                    modelWidth: 96,
+                    modelHeight: 76,
+                    modelLeftTopX: 37,
+                    modelLeftTopY: 352,
+                    markerLeftTopX: 400,
+                    markerLeftTopY: 615,
+                    markerSize: 80,
+                    attach: {
+                        src: 'Uploads/Audio/adjective.mp3',
+                        isAutoplay: false,
+                        imageTexSrc: 'Uploads/Image/blank.png'
+                    },
+                    clickEventCallback: function(e){
+                        if(e.clickCount%2){
+                            e.domElement.play();
+                            e.obj.material.map =  THREE.ImageUtils.loadTexture('Uploads/Image/continue.png');
+                        }
+                        else{
+                            e.obj.material.map =  THREE.ImageUtils.loadTexture('Uploads/Image/blank.png');
+                        }
+                    }
+                },
+
+                {
+                    modelType: AR.Models.ModelType.audio,
+                    modelWidth: 247,
+                    modelHeight: 406,
+                    modelLeftTopX: 140,
+                    modelLeftTopY: 250,
+                    markerLeftTopX: 400,
+                    markerLeftTopY: 615,
+                    markerSize: 80,
+                    attach: {
+                        src: 'Uploads/Audio/Text.mp3',
+                        isAutoplay: false,
+                        imageTexSrc: 'Uploads/Image/blank.png'
+                    },
+                    clickEventCallback: function(e){
+                        if(e.clickCount%2){
+                            e.domElement.play();
+                            e.obj.material.map =  THREE.ImageUtils.loadTexture('Uploads/Image/continue.png');
+                        }
+                        else{
+                            e.domElement.pause();
+                            e.obj.material.map =  THREE.ImageUtils.loadTexture('Uploads/Image/blank.png');
+                        }
+                    }
+                }
+
             ]
         }
 
     ];
 
+    this.getPage = function(markerId) {
+        for(var i in pages) {
+            if(pages[i].markerId == markerId) {
+                return pages[i];
+            }
+        }
+        return null;
+    };
+
     // imagine cup 视频
     this.loadPage1 = function() {
         Models.clearModels();
 
-        // movie
-        var movieHeight = 400;
-        var movieModel = Models.createModel(AR.Models.ModelType.video, movieHeight*672/378, movieHeight, {
-            src: 'Uploads/Movie/ImagineCup.mp4',
-            width: 672,
-            height: 378,
-            isLoop: true
-        }); //672 × 378
+        // 根据 markerId 获取 page，加载 models
+        var page = this.getPage(0);
+        if(page && page.models) {
+            for(var i in page.models) {
+                var modelData = page.models[i];
 
-        movieModel.obj.position.x = -240;
-        movieModel.obj.position.y = 296;
+                var model = Models.createModel(modelData.modelType, modelData.modelWidth, modelData.modelHeight, modelData.attach);
 
-        Models.addModel(0, movieModel, function(e){
-            if(e.clickCount%2)
-                e.domElement.play();
-            else
-                e.domElement.pause();
-        });
+                var scale = MARKER_SIZE/modelData.markerSize;
+                var offsetX = ((modelData.markerLeftTopX+modelData.markerSize/2)-(modelData.modelLeftTopX+modelData.modelWidth/2))*scale;
+                var offsetY = ((modelData.markerLeftTopY+modelData.markerSize/2)-(modelData.modelLeftTopY+modelData.modelHeight/2))*scale;
+
+                model.obj.position.x = -offsetX;
+                model.obj.position.y = offsetY;
+
+                Models.addModel(page.markerId, model, modelData.clickEventCallback);
+            }
+        }
     };
 
     // Car Models
     this.loadPage2 = function() {
         Models.clearModels();
 
-//        // movie
-//        var movieHeight = 400;
-//        var movieModel = Models.createModel(AR.Models.ModelType.video, movieHeight*592/252, movieHeight, {
-//            src: 'Uploads/Movie/wow.mp4',
-//            width: 592,
-//            height: 252,
-//            isLoop: true
-//        }); //592 × 252
-//
-//        movieModel.obj.position.x = 60;
-//        movieModel.obj.position.y = 300;
-//
-//        Models.addModel(4, movieModel, function(e){
-//            if(e.clickCount%2)
-//                e.domElement.play();
-//            else
-//                e.domElement.pause();
-//        });
+        // TODO
+        // 模型要用callback，暂时特殊处理...
+        // 并根据模型选择Loader...
 
         // obj3d
-//        var model3d = Models.createModel(AR.Models.ModelType.obj3d, 0, 0, {
-//            loader: new THREE.OBJLoader(),
-//            url:'Uploads/3DModel/male02/male02.obj',
-//            scale: 3,
-//            rotation: [Math.PI/2, 0, Math.PI],
-//            callback: function(model) {
-//                Models.addModel(0, model);
-//            }
-//        });
-
-        // 2 obj3d
         var model3d = Models.createModel(AR.Models.ModelType.obj3d, 0, 0, {
             loader: new THREE.BinaryLoader(),
             url:'Uploads/3DModel/veyron/VeyronNoUv_bin.js',
@@ -111,155 +216,30 @@ AR.Data = function(Models) {
             }
         });
 
-//        var model3d_second = Models.createModel(AR.Models.ModelType.obj3d, 0, 0, {
-//            loader: new THREE.BinaryLoader(),
-//            url:'Uploads/3DModel/gallardo/GallardoNoUv_bin.js',
-//            material: new THREE.MeshFaceMaterial([
-//                AR.Data.mlib[ "Pure chrome" ], 	// wheels chrome
-//                AR.Data.mlib[ "Black rough" ],   // tire
-//                AR.Data.mlib[ "Black glass" ], 	// windshield
-//                AR.Data.mlib[ "Green metal" ], 		// body
-//                AR.Data.mlib[ "Red glass" ],    	// back lights
-//                AR.Data.mlib[ "Yellow glass" ],  // front lights
-//                AR.Data.mlib[ "Dark chrome" ]	// windshield rim
-//            ]),
-//            scale: 0.5,
-//            rotation: [-Math.PI/2,0, 0],
-//            callback: function(model) {
-//                model.obj.position.x = -240;
-//                //model.obj.position.y = 356-100;
-//                Models.addModel(0, model);
-//            }
-//        });
-//
-//        var model3d_third = Models.createModel(AR.Models.ModelType.obj3d, 0, 0, {
-//            loader: new THREE.BinaryLoader(),
-//            url:'Uploads/3DModel/camaro/CamaroNoUv_bin.js',
-//            material: new THREE.MeshFaceMaterial([
-//                AR.Data.mlib[ "Orange" ], 			// car body
-//                AR.Data.mlib[ "Pure chrome" ], 		// wheels chrome
-//                AR.Data.mlib[ "Pure chrome" ], 		// grille chrome
-//                AR.Data.mlib[ "Dark chrome" ], 		// door lines
-//                AR.Data.mlib[ "Light glass" ], 		// windshield
-//                AR.Data.mlib[ "Gray shiny" ],        // interior
-//                AR.Data.mlib[ "Black rough" ],       // tire
-//                AR.Data.mlib[ "Fullblack rough" ],   // tireling
-//                AR.Data.mlib[ "Fullblack rough" ]    // behind grille
-//            ]),
-//            scale: 0.5,
-//            rotation: [-Math.PI/2,0, 0],
-//            callback: function(model) {
-//                model.obj.position.x = -240+100;
-//                //model.obj.position.y = 356;
-//                Models.addModel(0, model);
-//            }
-//        });
-
-
     };
 
     // 点读机
     this.loadPage3 = function() {
         Models.clearModels();
 
-        // audio-1
-        var audioModel1 = Models.createModel(AR.Models.ModelType.audio, 123, 129, {
-            src: 'Uploads/Audio/noun.mp3',
-            isAutoplay: false,
-            imageTexSrc: 'Uploads/Image/blank.png'
-        });
+        // 根据 markerId 获取 page，加载 models
+        var page = this.getPage(1);
+        if(page && page.models) {
+            for(var i in page.models) {
+                var modelData = page.models[i];
 
-        var scale = 120/80;
-        var offsetX = ((400+80/2)-(20+123/2))*scale;
-        var offsetY = ((615+80/2)-(221+129/2))*scale;
+                var model = Models.createModel(modelData.modelType, modelData.modelWidth, modelData.modelHeight, modelData.attach);
 
-        audioModel1.obj.position.x = -offsetX;
-        audioModel1.obj.position.y = offsetY;
+                var scale = MARKER_SIZE/modelData.markerSize;
+                var offsetX = ((modelData.markerLeftTopX+modelData.markerSize/2)-(modelData.modelLeftTopX+modelData.modelWidth/2))*scale;
+                var offsetY = ((modelData.markerLeftTopY+modelData.markerSize/2)-(modelData.modelLeftTopY+modelData.modelHeight/2))*scale;
 
-        Models.addModel(1, audioModel1,
-            function(e){
-                if(e.clickCount%2){
-                    e.domElement.play();
-                    e.obj.material.map =  THREE.ImageUtils.loadTexture('Uploads/Image/continue.png');
-                }
-                else{
-                    e.obj.material.map =  THREE.ImageUtils.loadTexture('Uploads/Image/blank.png');
-                }
+                model.obj.position.x = -offsetX;
+                model.obj.position.y = offsetY;
+
+                Models.addModel(page.markerId, model, modelData.clickEventCallback);
             }
-        );
-
-        // audio-2
-        var audioModel2 = Models.createModel(AR.Models.ModelType.audio, 116, 137, {
-            src: 'Uploads/Audio/pronoun.mp3',
-            isAutoplay: false,
-            imageTexSrc: 'Uploads/Image/blank.png'
-        });
-
-        scale = 120/80;
-        offsetX = ((400+80/2)-(383+116/2))*scale;
-        offsetY = ((615+80/2)-(262+137/2))*scale;
-
-        audioModel2.obj.position.x = -offsetX;
-        audioModel2.obj.position.y = offsetY;
-
-        Models.addModel(1, audioModel2, function(e){
-            if(e.clickCount%2){
-                e.domElement.play();
-                e.obj.material.map =  THREE.ImageUtils.loadTexture('Uploads/Image/continue.png');
-            }
-            else{
-                e.obj.material.map =  THREE.ImageUtils.loadTexture('Uploads/Image/blank.png');
-            }
-        });
-
-        // audio-3
-        var audioModel3 = Models.createModel(AR.Models.ModelType.audio, 96, 76, {
-            src: 'Uploads/Audio/adjective.mp3',
-            isAutoplay: false,
-            imageTexSrc: 'Uploads/Image/blank.png'
-        });
-
-        scale = 120/80;
-        offsetX = ((400+80/2)-(37+96/2))*scale;
-        offsetY = ((615+80/2)-(352+76/2))*scale;
-
-        audioModel3.obj.position.x = -offsetX;
-        audioModel3.obj.position.y = offsetY;
-
-        Models.addModel(1, audioModel3, function(e){
-            if(e.clickCount%2){
-                e.domElement.play();
-                e.obj.material.map =  THREE.ImageUtils.loadTexture('Uploads/Image/continue.png');
-            }
-            else{
-                e.obj.material.map =  THREE.ImageUtils.loadTexture('Uploads/Image/blank.png');
-            }
-        });
-
-        // audio-4
-        var audioModel4 = Models.createModel(AR.Models.ModelType.audio, 247, 406, {
-            src: 'Uploads/Audio/Text.mp3',
-            isAutoplay: false,
-            imageTexSrc: 'Uploads/Image/blank.png'
-        });
-
-        scale = 120/80;
-        offsetX = ((400+80/2)-(140+247/2))*scale;
-        offsetY = ((615+80/2)-(250+406/2))*scale;
-
-        audioModel4.obj.position.x = -offsetX;
-        audioModel4.obj.position.y = offsetY;
-
-        Models.addModel(1, audioModel4, function(e){
-            if(e.clickCount%2){
-                e.domElement.play();
-                e.obj.material.map =  THREE.ImageUtils.loadTexture('Uploads/Image/continue.png');
-            }
-            else{
-                e.domElement.pause();
-                e.obj.material.map =  THREE.ImageUtils.loadTexture('Uploads/Image/blank.png');
-            }
-        });
+        }
     };
 
 };
